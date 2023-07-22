71bc7965798ff06b2521b0676efb129c8ed0e64f
Melfsberg <69590682+Melfsberg@users.noreply.github.com>
2023-07-22 15:02:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
-import machine
 import network
 import socket
 import time
-import utime
-import webpage
 import struct
 import _thread
 import vattna
+import webpage
 import secrets
 
-rtc=machine.RTC()
-
-DEFTIMESTR=[]
-DEFDUR=[]
-DEFCNT=[]
 timestr=[]
 dur=[]
 cnt=[]
 systime=[]
+
+rtc=machine.RTC()
 
 def set_time():
     NTP_DELTA = 2208981600
@@ -37,6 +32,7 @@ def set_time():
     tm = time.gmtime(t)
     machine.RTC().datetime((tm[0], tm[1], tm[2], tm[6] , tm[3], tm[4], tm[5], 0))
     print("updated")
+    
 
 def control_thread():
     global dur,cnt,systime
@@ -46,35 +42,26 @@ def control_thread():
         systime="%02d" % rtc_time[0] + ":" + "%02d" % rtc_time[1] + ":" "%02d" % rtc_time[2]
         
         if systime in timestr:
-            slinga=timestr.index(systime)            
-            
+            slinga=timestr.index(systime)
             if int(dur[slinga])>0:
                 vattna.vattna(int(slinga+1),int(dur[slinga]))
                 cnt[slinga]=str(int(cnt[slinga])+1001)[1:4]
                 
-        time.sleep(.5)                        
-        
-def set_default():
-    global DEFTIMESTR,DEFDUR,DEFCNT    
-    DEFTIMESTR=['16:00:00','16:05:00','16:10:00','16:15:00']
-    DEFDUR=['060','050','045','000']
-    DEFCNT=['000','000','000','000']
+        time.sleep(.5)
 
+def set_default():
+    global timestr,dur,cnt
+
+    timestr=['16:00:00','16:05:00','16:10:00','16:15:00']
+    dur=['060','050','045','000']
+    cnt=['000','000','000','000']
+    
 def check_time_input(tid):
-    if len(tid)==8:
-        if tid[5]==tid[2]==':':
-            if int(tid[0:2]) < 24:                
-                if int(tid[3:5])<60:
-                    if int(tid[6:8])<60:                    
-                        return True
-                    else:
-                        return False    
-            else:
-                return False
-        else:
-            return False
+    if len(tid)==8 and tid[5]==tid[2]==':' and int(tid[0:2]) < 24 and \
+    int(tid[3:5])<60 and int(tid[6:8])<60:                    
+        return True
     else:
-        return False
+        return False  
 
 def check_dur_input(dur):
     try:
@@ -90,44 +77,50 @@ def test(slinga,cnt):
     vattna.vattna(int(slinga),10)
     return str(int(cnt)+1001)[1:4]
 
-wlan=network.WLAN(network.STA_IF)
+wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-wlan.connect(secrets.ssid,secrets.password)
+wlan.connect(secrets.ssid, secrets.password)
 
-wait=20
-while wait>0:
-    if wlan.status() < 0 or wlan.status() > 3:
+max_wait = 10
+while max_wait > 0:
+    if wlan.status() < 0 or wlan.status() >= 3:
         break
-    wait -=1
+    max_wait -= 1
     print('waiting for connection...')
-    time.sleep(.2)
-    
-if wlan.status() !=3:
-    raise RuntimeError('wifi connection failed')
+    time.sleep(1)
+
+if wlan.status() != 3:
+    raise RuntimeError('network connection failed')
 else:
     print('connected')
-    ip=wlan.ifconfig()[0]
-    print('IP: ', ip)
+    status = wlan.ifconfig()
+    print( 'ip = ' + status[0] )
+
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+
+s = socket.socket()
+s.bind(addr)
+s.listen(1)
+
+print('listening on', addr)
 
 _thread.start_new_thread(control_thread,())
 
-def serve(connection):
-    global timestr,dur,cnt,systime
-    init=True
-    i=[0]*12
-    
-    while True:
-        client = connection.accept()[0]
-        request = client.recv(1024)
+init=True
+i=[0]*12
+set_time()
+# Listen for connections
+while True:
+    try:
+        cl, addr = s.accept()
+        print('client connected from', addr)
+        request = cl.recv(1024)
         request = str(request)
-        try:
-            request.split()[1]
-        except IndexError:
-            pass
         
-        print(request)     
-        
-        if not init:
+        if init:
+            set_default()
+            init=False            
+        else:
             i[0]=request.find('time1=',0,160)
             i[1]=request.find('dur1=',0,160)
             i[2]=request.find('cnt1=',0,160)        
@@ -149,44 +142,24 @@ def serve(connection):
                 if check_dur_input(temp):
                     dur[int(n/3)]=temp
             
-        if request.find('/time',0,20)!=-1:            
-            print("update time")
-            set_time()
+            if request.find('/time',0,20)!=-1:            
+                print("update time")
+                set_time()
+               
+            if request.find('/reset',0,20) !=-1:
+                cnt=['000','000','000','000']
                 
-        if init:
-            set_default()
-            timestr=DEFTIMESTR
-            dur=DEFDUR
-            cnt=DEFCNT
-            init=False
-           
-        if request.find('/reset',0,20) !=-1:
-            cnt=['000','000','000','000']
-            
-        if request.find('/test',0,20)!=-1:        
-            temp=request.find('/test',0,20)
-            pos=int(request[temp+5])
-            cnt[(pos-1)]=test(pos,cnt[pos-1])
-           
-        print('---------------------------------------------------------')
-        print(timestr, '\n', dur, '\n',  cnt)
-        
+            if request.find('/test',0,20)!=-1:        
+                temp=request.find('/test',0,20)
+                pos=int(request[temp+5])
+                cnt[(pos-1)]=test(pos,cnt[pos-1])
+               
         html=webpage.webpage(systime,timestr,dur,cnt)
-        
-        client.send(html)
-        client.close()
-        
-def open_socket(ip):
-    address = (ip, 80)
-    connection = socket.socket()
-    connection.bind(address)
-    connection.listen(1)
-    print(connection)
-    return(connection)
-try:
-    if ip is not None:
-        connection=open_socket(ip)
-        serve(connection)
-except KeyboardInterrupt:
-    machine.reset()            
-            
+
+        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        cl.send(html)
+        cl.close()
+
+    except OSError as e:
+        cl.close()
+        print('connection closed')
